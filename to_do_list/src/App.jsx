b02d9cabc3cb4d44c7c770f91d9df50e02405409
Dranas786 <dranas2002@gmail.com>
2025-01-27 17:16:30 -0700

    Added persistance

--- to_do_list/src/App.jsx
+++ to_do_list/src/App.jsx
@@ -1,14 +1,19 @@
 import TodoInput from "./components/Todoinput";
 import TodoList from "./components/TodoList";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [todoValue, setTodoValue] = useState("");
   const [todos, setTodos] = useState([]);
 
+  function persistData(newList) {
+    localStorage.setItem("todos", JSON.stringify({ todos: newList }));
+  }
+
   function handleAddTodos(newTodo) {
     const newTodoList = [...todos, newTodo];
     setTodos(newTodoList);
+    persistData(newTodoList);
   }
 
   function handleDeleteTodo(index) {
@@ -16,6 +21,7 @@ function App() {
       return todoIndex != index;
     });
     setTodos(newToDoList);
+    persistData(newToDoList);
   }
 
   function handleEditTodo(index) {
@@ -23,6 +29,19 @@ function App() {
     setTodoValue(valueToBeEdited);
     handleDeleteTodo(index);
   }
+
+  useEffect(() => {
+    if (!localStorage) {
+      return;
+    }
+
+    let localTodos = localStorage.getItem("todos");
+    if (!localTodos) {
+      return;
+    }
+    localTodos = JSON.parse(localTodos).todos;
+    setTodos(localTodos);
+  }, []);
   return (
     <>
       <TodoInput
